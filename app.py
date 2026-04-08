524de9b1720c41ddf68e1b3909090426bf89907b
ashin333-cloud <ashinshaji333@gmail.com>
2026-04-08 11:04:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def analyzer_node(state: InsuranceState):
     """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
+        model="gemma-3n-e4b-it",
         contents=prompt,
         config=types.GenerateContentConfig(
             response_mime_type="application/json",
